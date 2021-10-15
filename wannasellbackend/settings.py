db1fb87ce5f125a8a3324f067787be0f0143ceec
BuildTools <unconfigured@null.spigotmc.org>
2021-10-15 17:39:01 +0200

    Setting default password to avoid test crash

--- wannasellbackend/settings.py
+++ wannasellbackend/settings.py
@@ -146,7 +146,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='wannasellteam@gmail.com')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default=None)
 
 if config('DJANGO_PRODUCTION_ENV', default=False, cast=bool):
     from .settings_prod import *
