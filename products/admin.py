0de884f69dedcaf41deedf2de8ad8b20ad54e2ad
BuildTools <unconfigured@null.spigotmc.org>
2021-10-19 18:03:41 +0200

    Registering 3 models on admin site.

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from products.models import Category, Product, Image
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Image)
