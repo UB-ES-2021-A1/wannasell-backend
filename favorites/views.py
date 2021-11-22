6e1c4df85e59f1eea425ef3006bc361444c67757
David Lopez <48055910+daload@users.noreply.github.com>
2021-11-22 17:10:21 +0100

    Feat: Get favorites of the user (#79)
    * - Added endpoint to get all favorite products of user
    
    * - Changed earlier commit so it makes more sense
    
    * - Added tests and removed unused imports

--- favorites/views.py
+++ favorites/views.py
@@ -10,9 +10,7 @@ from rest_framework.authentication import TokenAuthentication, SessionAuthentica
 from rest_framework.permissions import IsAuthenticated, BasePermission, SAFE_METHODS
 
 from favorites.models import Favorites
-from favorites.serializers import FavDataSerializer
 from products.models import Product
-from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -24,8 +22,25 @@ class ReadOnly(BasePermission):
 class FavoritesView(APIView):
     # serializer_class = ProductDataSerializer
 
-    permission_classes = [IsAuthenticated | ReadOnly]
+    permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication, SessionAuthentication]
+
+    def get(self, request):
+        try:
+            favs = Favorites.objects.filter(user=request.user)
+        except:
+            return Response("Couldn't retrieve favorites", status=status.HTTP_404_NOT_FOUND)
+
+        prod_list = favs.values_list('product__id', flat=True)
+        serialized_lst = []
+        try:
+            serialized_lst = [ProductDataSerializer(Product.objects.get(id=prod_id)).data for prod_id in prod_list]
+        except:
+            return Response("Couldn't retrieve product", status=status.HTTP_404_NOT_FOUND)
+
+        response_status = status.HTTP_200_OK if products else status.HTTP_204_NO_CONTENT
+        print(request.auth)
+        return Response(serialized_lst, status=response_status)
 
     def post(self, request, id=None):
         if id:
@@ -56,3 +71,12 @@ class FavoritesView(APIView):
 
             return Response(status=status.HTTP_200_OK)
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class FavoritesByUserView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
+
+
+
+
