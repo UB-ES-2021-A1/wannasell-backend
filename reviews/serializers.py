8e2215cef4a6ca6a63267210638892204c8d0231
David Lopez <48055910+daload@users.noreply.github.com>
2021-12-05 15:46:30 +0100

    Feat: Reviews View (#109)
    * - Added Reviews endpoint (GET, POST and PATCH)
    
    * - Modified PATCH so it gets the buyer/reviewer from the request isntead of the URL

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -7,3 +7,12 @@ class ReviewDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = ['id', 'reviewer', 'seller', 'message', 'val', 'check', 'created_at']
+
+    def update(self, instance, validated_data):
+        instance.reviewer = validated_data.get('reviewer', instance.reviewer)
+        instance.seller = validated_data.get('seller', instance.seller)
+        instance.message = validated_data.get('message', instance.message)
+        instance.val = validated_data.get('val', instance.val)
+        instance.check = validated_data.get('check', instance.check)
+        instance.save()
+        return instance
